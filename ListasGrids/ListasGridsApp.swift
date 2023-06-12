6117cc779a7635594591def29ca116c5885de84f
YerayCastro <112658475+YerayCastro@users.noreply.github.com>
2023-06-12 13:17:29 +0200

    commit

---
+++ ListasGrids/ListasGridsApp.swift
@@ -0,0 +1,18 @@
+//
+//  ListasGridsApp.swift
+//  ListasGrids
+//
+//  Created by Yery Castro on 17/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ListasGridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //ContentView()
+            GridList()
+        }
+    }
+}
